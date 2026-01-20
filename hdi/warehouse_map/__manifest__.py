eec82f8934e95ac91eb1841ddc25e5c28688ffd3
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2026-01-20 13:59:27 +0700

    done

--- hdi/warehouse_map/__manifest__.py
+++ hdi/warehouse_map/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Warehouse Map Layout',
-    'version': '17.0.1.6.4',
+    'version': '18.0.1.0.0',
     'category': 'Inventory/Inventory',
     'summary': 'Sơ đồ kho - Cell updates instantly',
     'description': """
