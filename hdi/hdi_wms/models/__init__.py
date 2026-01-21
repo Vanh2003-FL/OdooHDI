162a7b9fec367841748854d0f453002bd3dcb044
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2026-01-21 14:33:58 +0700

    dc

---
+++ hdi/hdi_wms/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from . import warehouse_layout
+from . import warehouse_3d_service
+from . import hdi_batch
+from . import hdi_putaway_suggestion
+from . import hdi_loose_line
+from . import hdi_picking_list
+from . import stock_picking
+from . import stock_move
+from . import stock_location
+from . import stock_quant
+from . import product_product
---
+++ hdi/warehouse_map/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import warehouse_map
+from . import stock_location
+from . import blocked_cell
+from . import res_config_settings
