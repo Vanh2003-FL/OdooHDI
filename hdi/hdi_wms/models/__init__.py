247803b5631c30febf91706405a476a4a3070ba7
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2026-01-21 11:53:14 +0700

    done

--- hdi/hdi_wms/models/__init__.py
+++ hdi/hdi_wms/models/__init__.py
@@ -4,8 +4,4 @@ from . import hdi_batch
 from . import hdi_putaway_suggestion
 from . import hdi_loose_line
 from . import hdi_picking_list
-from . import stock_picking
-from . import stock_move
-from . import stock_location
-from . import stock_quant
-from . import product_product
+from . import hdi_warehouse_layout
