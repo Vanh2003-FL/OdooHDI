597d98b95a66bbff1538ecf91b919c1e0727950b
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2026-01-26 15:50:09 +0700

    ok

--- hdi/warehouse_map/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import warehouse_map
-from . import warehouse_map_3d
-from . import stock_location
-from . import stock_picking
-from . import blocked_cell
--- hdi/warehouse_map/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import location_action_wizard
-from . import assign_lot_position_wizard
-from . import assign_lot_position_3d_wizard
-from . import block_cell_wizard
-from . import move_line_warehouse_map_wizard
